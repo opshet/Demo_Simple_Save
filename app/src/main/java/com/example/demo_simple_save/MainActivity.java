efb65628280a92702878d605f3cdf0f824265bf7
marcel <marcellim26@gmail.com>
2021-07-08 11:17:40 +0800

    Initial commit

---
+++ app/src/main/java/com/example/demo_simple_save/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.demo_simple_save;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        SharedPreferences prefs= getPreferences(MODE_PRIVATE);
+        String msg=prefs.getString("greetings", "No greetings name");
+        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        SharedPreferences prefs=getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor prefEdit= prefs.edit();
+        prefEdit.putString("greetings", "Hello");
+        prefEdit.commit();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
+
